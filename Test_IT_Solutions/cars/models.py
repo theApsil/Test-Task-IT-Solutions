3b5fe42be0f7408f174fc781a365427007a9d277
Danil Goncharuk <goncharuk.dm@students.dvfu.ru>
2024-08-06 17:05:10 +1000

    Create: Model of car

--- Test_IT_Solutions/cars/models.py
+++ Test_IT_Solutions/cars/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from Test_IT_Solutions.consts import FUEL_CHOICES, TRANSMISSION_CHOICES
 
-# Create your models here.
+
+class Car(models.Model):
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    fuel_type = models.CharField(max_length=20, choices=FUEL_CHOICES)
+    transmission = models.CharField(max_length=20, choices=TRANSMISSION_CHOICES)
+    mileage = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
